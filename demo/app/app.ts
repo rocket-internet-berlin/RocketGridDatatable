0387863ab7bba9b02cea3389aaa7cc6e40a48d60
Konrad Cerny <konrad.cerny@rocket-internet.de>
2016-04-26 11:05:11 +0200

    Reset offset on search

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -5,6 +5,8 @@ import UserService from './usersService';
 
 import UserPresentationService from './usersPresentationService';
 
+import {EVENT_PAGE_CHANGED_DATA_TABLE} from '../../dist/events';
+
 let datatableDemoApplication = angular.module('datatableDemo', [
     'rocket-grid-datatable',
 ]);
@@ -12,5 +14,5 @@ datatableDemoApplication.constant('paginationLimitPerPage', 5);
 datatableDemoApplication.service('UserService', UserService);
 datatableDemoApplication.service('UsersPresentationService', UserPresentationService);
 datatableDemoApplication.controller('DemoCtrl', ($scope: ng.IScope) => {
-    $scope.$on('datatable-page-changed', () => console.log('ctrl: page was changed'));
+    $scope.$on(EVENT_PAGE_CHANGED_DATA_TABLE, () => console.log('ctrl: page was changed'));
 });
